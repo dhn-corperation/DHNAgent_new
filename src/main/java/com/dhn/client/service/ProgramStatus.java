4655e8192a77b2e779a4d96de8df20a59bde90f6
mango7431 <rsm5065@naver.com>
2025-10-30 13:17:39 +0900

    programstatus friend remove

--- src/main/java/com/dhn/client/service/ProgramStatus.java
+++ src/main/java/com/dhn/client/service/ProgramStatus.java
@@ -31,9 +31,7 @@ public class ProgramStatus implements CommandLineRunner, ApplicationListener<Con
         BMSendRequest.isStart = false;
         BMCSendRequest.isStart = false;
         BMDSendRequest.isStart = false;
-        FTSendRequest.isStart = false;
         OTPSendRequest.isStart = false;
-        KFSendRequest.isStart = false;
         KAOLogMove.isStart = false;
         MSGLogMove.isStart = false;
         ResultReq.isStart = false;
