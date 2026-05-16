bf16b1fd90c62cda03ff029437d005b3a89f39e4
mango7431 <rsm5065@naver.com>
2026-05-16 13:25:43 +0900

    result E,G add

--- src/main/java/com/dhn/client/controller/ResultReq.java
+++ src/main/java/com/dhn/client/controller/ResultReq.java
@@ -142,7 +142,9 @@ public class ResultReq implements ApplicationListener<ContextRefreshedEvent>{
 					|| ent.getString("message_type").toUpperCase().startsWith("F")
 					|| ent.getString("message_type").toUpperCase().startsWith("B")
 					|| ent.getString("message_type").toUpperCase().startsWith("C")
-					|| ent.getString("message_type").toUpperCase().startsWith("D")){
+					|| ent.getString("message_type").toUpperCase().startsWith("D")
+					|| ent.getString("message_type").toUpperCase().startsWith("E")
+					|| ent.getString("message_type").toUpperCase().startsWith("G")){
 				// 비즈메세지
 				kao_ml.setMsgid(ent.getString("msgid"));
 				kao_ml.setMsg_table(msg_table);
